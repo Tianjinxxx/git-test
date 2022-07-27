caa9474851a4ef3a2ee926a11d7ed66bdcf790d5
Tianjinxxx <2160543832@qq.com>
2022-07-27 19:11:17 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @Description:
+ * @NAME: GitTest
+ * @DATE: 2022/7/27 19:05
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
